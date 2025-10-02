964453daf8cb70a5e4b1d3ef6241b8f4fe79388f
LuccaSantoro <101141030+LuccaSantoro@users.noreply.github.com>
2025-10-02 13:34:38 -0300

    Atualizar o app.py

--- Exercícios 1 ao 45/Exercício7/app.py
+++ Exercícios 1 ao 45/Exercício7/app.py
@@ -1,26 +1,9 @@
 #Receba os valores do comprimento, largura e altura de um paralelepípedo. Calcule e mostre seu volume.
 
-print("Cálculo do volume de um paralelepípedo")
+comp = float(input("Digite o comprimento: "))
+larg = float(input("Digite a largura: "))
+alt = float(input("Digite a altura: "))
 
-try:
-    # 1. Recebe a entrada do valor do comprimento
-    comprimento = float(input("Insira o valor do comprimento: "))
-    
-    # 2. Recebe a entrada do valor da largura
-    largura = float(input("Insira o valor da largura: "))
-    
-    # 3. Recebe a entrada do valor da altura
-    altura = float(input("Insira o valor da altura: "))
+volume = comp * larg * alt
 
-except ValueError:
-    print("Entrada inválida. Por favor, insira apenas números.")
-    exit()
-
-# --- Lógica de Cálculo do Volume ---
-
-volume = comprimento * largura * altura
-print(f"O volume do paralelepípedo é: {volume:.2f}")
-
-# --- Fim da Lógica de Cálculo ---
-
-#fim
+print("O volume e:", volume)
