5551e0c8bf776115a530b94b09e5045bd2e7d679
LuccaSantoro <101141030+LuccaSantoro@users.noreply.github.com>
2025-10-02 13:48:41 -0300

    Atualizar o app.py

--- Exercícios 1 ao 45/Exercício15/app.py
+++ Exercícios 1 ao 45/Exercício15/app.py
@@ -1,23 +1,9 @@
 #Receba os valores de 2 catetos de um triângulo retângulo. Calcule e mostre a hipotenusa.
 
-print("Cálculo da hipotenusa de um triângulo retângulo")
+cateto1 = float(input("Digite o valor de um cateto: "))
+cateto2 = float(input("Digite o valor do outro cateto: "))
 
-try:
-    # 1. Recebe a entrada do valor do primeiro cateto
-    cateto1 = float(input("Insira o valor do primeiro cateto: "))
-    
-    # 2. Recebe a entrada do valor do segundo cateto
-    cateto2 = float(input("Insira o valor do segundo cateto: "))
+soma_quadrados = (cateto1 * cateto1) + (cateto2 * cateto2)
+hipotenusa = soma_quadrados ** 0.5
 
-except ValueError:
-    print("Entrada inválida. Por favor, insira apenas números.")
-    exit()
-
-# --- Lógica de Cálculo da Hipotenusa ---
-
-hipotenusa = (cateto1**2 + cateto2**2)**0.5
-print(f"A hipotenusa do triângulo retângulo com catetos {cateto1} e {cateto2} é: {hipotenusa:.2f}")
-
-# --- Fim da Lógica de Cálculo ---
-
-#fim
+print("O valor da hipotenusa e:", hipotenusa)
