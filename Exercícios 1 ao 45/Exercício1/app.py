7b1ba552e975a91845e193d7f419fe005881c872
LuccaSantoro <101141030+LuccaSantoro@users.noreply.github.com>
2025-10-02 13:28:43 -0300

    Atualizar o app.py

--- Exercícios 1 ao 45/Exercício1/app.py
+++ Exercícios 1 ao 45/Exercício1/app.py
@@ -1,21 +1,7 @@
 #Coletar o valor do lado de um quadrado, calcular sua área e apresentar o resultado.
 
-print("Cálculo da área de um quadrado")
-
-try:
-
-    # 1. Recebe a entrada do valor do lado
-    lado = float(input("Insira o valor do lado do quadrado: "))
-
-except ValueError:
-    print("Entrada inválida. Por favor, insira apenas números.")
-    exit()
-
-# --- Lógica de Cálculo da Área  ---
+lado = float(input("Digite o valor do lado: "))
 
 area = lado * lado
-print(f"A área do quadrado é: {area:.2f}")
 
-# --- Fim da Lógica de Cálculo ---
-
-#fim
+print("A area do quadrado e: ", area)
