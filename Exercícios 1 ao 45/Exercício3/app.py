18631ae9af6ae2054904f6fda4dfeab9253528f1
LuccaSantoro <101141030+LuccaSantoro@users.noreply.github.com>
2025-10-02 13:30:30 -0300

    Atualizar o app.py

--- Exercícios 1 ao 45/Exercício3/app.py
+++ Exercícios 1 ao 45/Exercício3/app.py
@@ -1,21 +1,8 @@
 #Receba a base e a altura de um triângulo. Calcule e mostre a sua área.
-print("Cálculo da área de um triângulo")
 
-try:
-    # 1. Recebe a entrada do valor da base
-    base = float(input("Insira o valor da base do triângulo: "))
-    
-    # 2. Recebe a entrada do valor da altura
-    altura = float(input("Insira o valor da altura do triângulo: "))
-
-except ValueError:
-    print("Entrada inválida. Por favor, insira apenas números.")
-    exit()
-# --- Lógica de Cálculo da Área ---
+base = float(input("Digite a base: "))
+altura = float(input("Digite a altura: "))
 
 area = (base * altura) / 2
-print(f"A área do triângulo é: {area:.2f}")
 
-# --- Fim da Lógica de Cálculo ---
-
-#fim
+print("A area do triangulo e:", area)
