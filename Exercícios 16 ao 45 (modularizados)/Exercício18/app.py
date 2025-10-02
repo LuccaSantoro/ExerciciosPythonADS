7f5c84c2ee23b84942725443989e4de55e830b9b
LuccaSantoro <101141030+LuccaSantoro@users.noreply.github.com>
2025-10-02 17:20:03 -0300

    Update app.py

--- Exercícios 16 ao 45 (modularizados)/Exercício18/app.py
+++ Exercícios 16 ao 45 (modularizados)/Exercício18/app.py
@@ -1,55 +1,27 @@
 # Receba 2 valores inteiros. Calcule e mostre o resultado da diferença do maior pelo menor valor.
 
-def obter_valores():
-    """
-    Solicita dois números inteiros ao usuário com tratamento de erro.
-    """
-    print("Cálculo da diferença entre dois números inteiros")
-    while True:
-        try:
-            # 1. Recebe a entrada do primeiro número
-            valor1 = int(input("Insira o valor do primeiro número inteiro: "))
-            
-            # 2. Recebe a entrada do segundo número
-            valor2 = int(input("Insira o valor do segundo número inteiro: "))
-            
-            return valor1, valor2
-        
-        except ValueError:
-            # Tratativa de erro para entrada não-numérica
-            print("Entrada inválida. Por favor, insira apenas números inteiros.")
+#Declarar
+num1 = 0
+num2 = 0
+diferenca = 0
 
-def calcular_e_mostrar_diferenca(valor1, valor2):
-    """
-    Calcula a diferença entre o maior e o menor valor e exibe o resultado.
-    """
-    # Determina o maior e o menor
-    if valor1 > valor2: 
-        maior = valor1
-        menor = valor2
-    elif valor2 > valor1:
-        maior = valor2
-        menor = valor1
-    else:
-        # Se os valores são iguais
-        print("Os dois valores são iguais. A diferença é 0.")
-        return
+#procedimento
+def Leitura():
+  global num1, num2
+  num1 = int(input("Digite o primeiro numero: "))
+  num2 = int(input("Digite o segundo numero: "))
 
-    # Calcula a diferença
-    diferenca = maior - menor
-    
-    # Exibe o resultado
-    print(f"A diferença entre {maior} e {menor} é: {diferenca}")
+#procedimento
+def CalculaDiferenca():
+  global num1, num2, diferenca
+  if num1 > num2:
+    diferenca = num1 - num2
+  else:
+    diferenca = num2 - num1
 
-def main():
-    """
-    Função principal que coordena a execução do programa.
-    """
-    # 1. Obtém os valores
-    val1, val2 = obter_valores()
-    
-    # 2. Calcula e exibe a diferença
-    calcular_e_mostrar_diferenca(val1, val2)
+#Início
+Leitura()
+CalculaDiferenca()
+print("A diferenca do maior para o menor e:", diferenca)
 
-if __name__ == "__main__":
-    main()
+#fim
