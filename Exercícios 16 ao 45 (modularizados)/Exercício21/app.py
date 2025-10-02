ea166b045c6bc20ea5bbc2e86a18f48385dc7499
LuccaSantoro <101141030+LuccaSantoro@users.noreply.github.com>
2025-10-02 17:22:28 -0300

    Update app.py

--- Exercícios 16 ao 45 (modularizados)/Exercício21/app.py
+++ Exercícios 16 ao 45 (modularizados)/Exercício21/app.py
@@ -1,23 +1,39 @@
-def obter_nota(bimestre):
-    return float(input(f"Insira a nota do {bimestre}º bimestre:"))
+# Calcule a nota média de um aluno em 4 bimestres
 
-def calcular_media(notas):
-    return sum(notas) / len(notas)
+#Declarar
+nota1 = 0.0
+nota2 = 0.0
+nota3 = 0.0
+nota4 = 0.0
+media_final = 0.0
 
-def mostrar_resultado(media):
-    print(f"A média do aluno é {media:.2f}")
-    if media >= 6:
-        print("APROVADO")
-    elif media < 3:
-        print("RETIDO")
-    else:
-        print("EXAME")
+#procedimento
+def Leitura():
+  global nota1, nota2, nota3, nota4
+  nota1 = float(input("Digite a nota do 1º bimestre: "))
+  nota2 = float(input("Digite a nota do 2º bimestre: "))
+  nota3 = float(input("Digite a nota do 3º bimestre: "))
+  nota4 = float(input("Digite a nota do 4º bimestre: "))
 
-def main():
-    print("Calcule a nota média de um aluno em 4 bimestres")
-    notas = [obter_nota(i) for i in range(1, 5)]
-    media = calcular_media(notas)
-    mostrar_resultado(media)
+#procedimento
+def Calculo():
+  global nota1, nota2, nota3, nota4, media_final
+  media_final = (nota1 + nota2 + nota3 + nota4) / 4
 
-if __name__ == "__main__":
-    main()
+#procedimento
+def Resultado():
+  global media_final
+  print("A media final e:", media_final)
+  if media_final >= 6.0:
+    print("APROVADO")
+  elif media_final >= 3.0:
+    print("EXAME")
+  else:
+    print("RETIDO")
+
+#Início
+Leitura()
+Calculo()
+Resultado()
+
+#fim
