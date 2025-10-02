29bfc69bd4325df02dfe512db10dccc943542060
LuccaSantoro <101141030+LuccaSantoro@users.noreply.github.com>
2025-10-02 17:31:37 -0300

    Update app.py

--- Exercícios 16 ao 45 (modularizados)/Exercício28/app.py
+++ Exercícios 16 ao 45 (modularizados)/Exercício28/app.py
@@ -1,28 +1,39 @@
-print("Calcule o novo preço de um produto")
+# Receba o preço atual e a média mensal de um produto. Calcule e mostre o novo preço sabendo que:
+# Venda Mensal Preço Atual Preço Novo
+# < 500 < 30 + 10%
+# >= 500 e < 1000 >= 30 e < 80 +15%
+# >= 1000 >= 80 - 5%
+# Obs.: para outras condições, preço novo será igual ao preço atual
 
+#Declarar
+preco_atual = 0
+venda_mensal = 0
 
-#declarar
+#procedimento
+def Leitura():
+  global preco_atual, venda_mensal
+  preco_atual = float(input("Insira o valor atual do produto: "))
+  venda_mensal = int(input("Insira a venda mensal do produto: "))
 
-preço_atual = float(input("Insira o valor atual do produto:"))
-venda_mensal = int(input("Insira a venda mensal do produto"))
+#procedimento
+def CalculaReajuste():
+  global preco_atual, venda_mensal
+  novo_preco = 0
+  
+  if venda_mensal < 500 and preco_atual < 30:
+    novo_preco = preco_atual * 1.1
+    print("O novo preco e:", novo_preco)
+  elif (venda_mensal >= 500 and venda_mensal < 1000) and (preco_atual >= 30 and preco_atual < 80):
+    novo_preco = preco_atual * 1.15
+    print("O novo preco e:", novo_preco)
+  elif venda_mensal >= 1000 and preco_atual >= 80:
+    novo_preco = preco_atual * 0.95
+    print("O novo preco e:", novo_preco)
+  else:
+    print("O preco nao sofreu reajuste:", preco_atual)
 
-def novo_preço():
-    if venda_mensal < 500 and preço_atual < 30:
-        reajuste = preço_atual * 1.1
-    elif venda_mensal >= 500 and venda_mensal <1000 and preço_atual >= 30 and preço_atual <80:
-        reajuste = preço_atual * 1.15
-    elif venda_mensal >= 1000 and preço_atual >= 80:
-        reajuste = preço_atual * 0.95
-    elif preço_atual < 0 or venda_mensal <0:
-        print("Valor inferior a 0, insira outro número")
-        return None
-    else:
-        reajuste = preço_atual
-    return reajuste
+#Início
+Leitura()
+CalculaReajuste()
 
-#inicio
-
-reajuste = novo_preço()
-if reajuste is not None:
-    print(f"O novo valor do produto será de R${reajuste:.2f}")
-print(f"O novo valor do produto será de R${reajuste:.2f}")
+#fim
