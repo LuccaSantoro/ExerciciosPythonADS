0f04af654a36ee1453543bbd5106d8d938defbc8
LuccaSantoro <101141030+LuccaSantoro@users.noreply.github.com>
2025-10-02 17:30:15 -0300

    Update app.py

--- Exercícios 16 ao 45 (modularizados)/Exercício27/app.py
+++ Exercícios 16 ao 45 (modularizados)/Exercício27/app.py
@@ -1,42 +1,29 @@
-print("Calcule a velocidade média(km/h) de um circuito")
+#Receba o número de voltas, a extensão do circuito (em metros) e o tempo de
+duração (minutos). Calcule e mostre a velocidade média em km/h.
 
-#declarar
+#Declarar
+voltas = 0
+circuito_m = 0
+tempo_min = 0
+velocidade_final = 0
 
-def percorrido_km():
-    while True:
-        try:
-            voltas = float(input("Insira a quantidade de voltas:"))
-            if voltas <= 0:
-                print("A quantidade de voltas deve ser maior que zero.")
-                continue
-            tamanho_circuito = float(input("Insira o tamanho do circuito em metros:"))
-            if tamanho_circuito <= 0:
-                print("O tamanho do circuito deve ser maior que zero.")
-                continue
-            total_percorrido = voltas * (tamanho_circuito/1000)
-            return total_percorrido
-        except ValueError:
-            print("Por favor, insira um número válido.")
+#procedimento
+def Leitura():
+  global voltas, circuito_m, tempo_min
+  voltas = int(input("Digite o numero de voltas: "))
+  circuito_m = float(input("Digite a extensao do circuito em metros: "))
+  tempo_min = int(input("Digite o tempo em minutos: "))
 
-def converter_horas():
-    while True:
-        try:
-            minutos = int(input("Insira a duração do trajeto em minutos:"))
-            if minutos <= 0:
-                print("A duração deve ser maior que zero.")
-                continue
-            horas = minutos/60
-            return horas
-        except ValueError:
-            print("Por favor, insira um número válido.")
+#procedimento
+def Calculo():
+  global voltas, circuito_m, tempo_min, velocidade_final
+  distancia_km = (voltas * circuito_m) / 1000
+  tempo_h = tempo_min / 60
+  velocidade_final = distancia_km / tempo_h
 
-#inicio
-
-distancia = percorrido_km()
-tempo = converter_horas()
-
-velocidade_media = distancia/tempo
-
-print(f"A velocidade média foi de {velocidade_media:.2f} km/h")
+#Início
+Leitura()
+Calculo()
+print("A velocidade media foi de", velocidade_final, "km/h")
 
 #fim
